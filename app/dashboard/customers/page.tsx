442df12d056405d04d995c739ad77fbdb80003de
Cristian Arias <javier.0813@hotmail.com>
2024-03-22 09:06:58 -0500

    Nextjs-dashboard

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function CustomersPage() {
+  return <h1>Esta página es el contenido del customers</h1>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,29 @@
+import '@/app/ui/global.css';
+import { inter } from './ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <footer className="flex items-center justify-center py-5">
+          Heco con 💕 por la gente vercel
+        </footer>
+      </body>
     </html>
   );
 }
